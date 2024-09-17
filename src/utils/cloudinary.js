f3e89883bdbd10285e8cd5a0815824f1e3367c9e
zeeshan shaikh <skzeeshan10011@gmail.com>
2024-09-17 11:06:19 +0530

    cloudinary added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRETS,
+});
+
+
+const uploadOnCloudinary = async (filePath) => {
+    try {
+        if(!filePath) return null
+        const response = await cloudinary.uploader.upload(filePath, {resource_type:"auto"})
+        console.log("file has been successfully uploaded", response.url);
+        return response;
+        
+    } catch (error) {
+        fs.unlinkSync(filePath)
+        return null;
+    }
+}
+
+
+export {uploadOnCloudinary}
